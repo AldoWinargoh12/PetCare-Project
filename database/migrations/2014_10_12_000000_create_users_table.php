ee81fc8c29831d7592385d0e6587a34849f4c315
s3733468 <s3733468@student.rmit.edu.au>
2019-09-29 15:20:55 +1000

    waghul-merge
    Login,Registration with pet registration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+            $table->bigIncrements('id');
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+        
         });
     }
 
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,54 @@
+
+@extends('layouts.app')
+
+@section('content')
+  
+          
+            <h5 class="card-title text-center">{{ __('Login') }}</h5>
+              <form class="form-signin" method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="form-label-group">
+                  <input  type="text" id="inputEmail" class="form-control" placeholder="Email address" {{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus >
+                  <label for="inputemail" >Email/Username</label>
+                  
+                      @error('email')
+                      <span  role="alert">
+                        <strong>{{ $message }}</strong>
+                      </span>
+                      @enderror
+                </div>
+                <div class="form-label-group" >
+                  <input id="inputpassword" class="form-control" type="password" placeholder="Password" @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                  <label for="inputpassword" >{{ __('Password') }}</label>
+                  
+                    @error('password')
+                      <span  role="alert">
+                          <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                                
+                </div>
+                <div class="custom-control custom-checkbox mb-3">
+                  <input  type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }} class="custom-control-input" id="customCheck1">
+                  <label class="custom-control-label" for="customCheck1">
+                    {{ __('Remember Me') }}
+                  </label>
+                </div>
+                <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" >{{ __('Login') }}</button>
+                  @if (Route::has('password.request'))
+                    <a  href="{{ route('password.request') }}">
+                      {{ __('Forgot Your Password?') }}
+                    </a>
+                  @endif
+              </form>
+           
+    
+  
+  
+
+
+
+@endsection
+
+
+
