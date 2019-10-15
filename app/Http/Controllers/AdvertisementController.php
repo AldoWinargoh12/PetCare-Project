e8c6b11ee43ab9cc214ee4d9b37850deef6ff6ac
[Anak Agung Bagus Bayu Andika Maheswara] <bayumaheswara@gmail.com>
2019-10-15 17:09:22 +1100

    updated branch

---
+++ app/Http/Controllers/AdvertisementController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Advertisement;
+
+class AdvertisementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $advertisements = Advertisement::all()->toArray();
+        return view('pageTemplate.showAd', compact('advertisements'));
+    }
+    
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('pageTemplate.createAd');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+        'title'       => 'required',
+        'price'       => 'required',
+        'location'    => 'required',
+        'duration'    => 'required',
+        'description' => 'required'
+        ]);
+
+        $advertisements = new Advertisement([
+            'title'        =>   $request->get('title'),
+            'price'        =>   $request->get('price'),
+            'location'     =>   $request->get('location'),
+            'duration'     =>   $request->get('duration'),
+            'description'  =>   $request->get('description')
+        ]);
+        $advertisements->save();
+        return view('pageTemplate.createAd');
+       // return redirect()->route('pageTemplate.createAd')->with('success', 'Data Added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
